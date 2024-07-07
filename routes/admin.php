caf5e3f644d7a32dee5d59827aebb9810df66207
padhukneythra <padmavathy20may@gmail.com>
2024-07-07 06:10:40 +0530

    First commit

---
+++ routes/admin.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Http\Controllers\Backend\AdminController;
+use App\Http\Controllers\backend\CategoryController;
+use App\Http\Controllers\Backend\ChildCategoryController;
+use App\Http\Controllers\Backend\ProfileController;
+use App\Http\Controllers\Backend\SliderController;
+use App\Http\Controllers\Backend\SubCategoryController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('dashboard', [AdminController::class, 'dashboard'])->name('dashboard');
+    
+/** profile routes */
+
+Route::get('profile', [ProfileController::class, 'index'])->name('profile');
+Route::post('profile/update', [ProfileController::class, 'updateProfile'])->name('profile.update');
+Route::post('profile/update/password', [ProfileController::class, 'updatePassword'])->name('password.update');
+
+/** Slider Route */
+Route::resource('slider', SliderController::class);
+
+/** Category Route */
+Route::put('change-status', [CategoryController::class, 'changeStatus'])->name('category.change-status');
+Route::resource('category', CategoryController::class);
+
+/** Sub Category Route */
+Route::put('sub-category/change-status', [SubCategoryController::class, 'changeStatus'])->name('sub-category.change-status');
+Route::resource('sub-category', SubCategoryController::class);
+
+/** Child Category Route */
+Route::put('child-category/change-status', [ChildCategoryController::class, 'changeStatus'])->name('child-category.change-status');
+Route::resource('child-category', ChildCategoryController::class);
